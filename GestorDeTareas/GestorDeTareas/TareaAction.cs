45fd512ee674fbc5ff47749b2161770040b5d201
anaya <anaya@LAPTOP-9FAIHIOC>
2026-04-13 19:48:54 +0200

    Estoy hasta los huevos de no poder hacer que compile

--- GestorDeTareas/GestorDeTareas/TareaAction.cs
+++ GestorDeTareas/GestorDeTareas/TareaAction.cs
@@ -7,7 +7,7 @@ namespace Gestor_de_Tareas
 {
     internal class TareaAction : Tarea
     {
-        public TareaAction(int id, string titulo, string descripcion, string responsable, DateTime fechaCreacion, DateTime fechaLimite, DateTime? fechaFinTarea, PrioridadTarea prioridad, EstadoTarea estado, string? motivacionCancelacion, List<TareaDto>? subTareas) : base(id, titulo, descripcion, responsable,fechaCreacion, fechaLimite, prioridad, estado, motivacionCancelacion, subTareas)
+        public TareaAction(int id, string titulo, string descripcion, string responsable, DateTime fechaCreacion, DateTime fechaLimite, DateTime? fechaFinTarea, PrioridadTarea prioridad, EstadoTarea estado, string? motivacionCancelacion, List<Tarea>? subTareas) : base(id, titulo, descripcion, responsable,fechaCreacion, fechaLimite, prioridad, estado, motivacionCancelacion, subTareas)
         {
         }
 
