dd199ffc625c53fbc8d59746806fea4cdbb97801
Miguel Cervera <l.riusaki15@gmail.com>
2026-04-13 09:29:30 +0200

    Creado el lector de Json para nuevos datos

---
+++ Gestor de Tareas/Gestor de Tareas/TareasJson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Gestor_de_Tareas
+{
+    internal class TareasJson
+    {
+        const string ruta = "Tareas.json";
+        public void GuardarDatosJson(TareaDto tareas)
+        {
+            string json = JsonSerializer.Serialize(tareas);
+            File.WriteAllText("Tareas.json", json);
+        }
+         public List<TareaDto> RecuperarDatos()
+        {
+            var opciones = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true
+            };
+            string json = File.ReadAllText(ruta);
+            return JsonSerializer.Deserialize<List<TareaDto>>(json, opciones) ?? new();
+        }
+    }
+}
