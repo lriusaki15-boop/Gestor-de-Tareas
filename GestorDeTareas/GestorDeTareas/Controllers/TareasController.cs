546d6ec5583f0f5d8562b3b59d625685ee45713f
Miguel Cervera <l.riusaki15@gmail.com>
2026-04-30 13:41:44 +0200

    inicio de configuracion de la api y del swaguer, terminar en casa para que inicie el swaguer bien

---
+++ GestorDeTareas/GestorDeTareas/Controllers/TareasController.cs
@@ -0,0 +1,17 @@
+﻿using GestorDeTareas.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorDeTareas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TareasController : ControllerBase
+    {
+        private readonly GestorTareasContext _context;
+
+        public TareasController(GestorTareasContext context)
+        {
+            _context = context;
+        }
+    }
+}
