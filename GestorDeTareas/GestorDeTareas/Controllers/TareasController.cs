67161c8c15b0dc052d51291672e89341e916fec5
Miguel Cervera <l.riusaki15@gmail.com>
2026-05-11 13:52:44 +0200

    Solucionado problema de tema de taresas y puestos nombre y explicaciones en el swaguer de las mismas. Iniciado los endpoits de usuario

--- GestorDeTareas/GestorDeTareas/Controllers/TareasController.cs
+++ GestorDeTareas/GestorDeTareas/Controllers/TareasController.cs
@@ -13,7 +13,11 @@ namespace GestorDeTareas.Controllers
         private readonly TareasServices _servicio;
         public TareasController(TareasServices servicio) => _servicio = servicio;
 
-        [HttpGet("Obtener-Tareas-Por-ID/{id}")]
+        /// <summary>
+        /// Obtiene una tarea en especifica.
+        /// </summary>
+        /// <returns>Devuelve la informacion de 1 tarea al completo.</returns>
+        [HttpGet("ObtenerTareasPorID/{id}")]
         public IActionResult ObtenerTareaPorId(int id)
         {
             var tarea = _servicio.ObtenerPorId(id);
@@ -24,7 +28,11 @@ namespace GestorDeTareas.Controllers
             return Ok(tarea);
         }
 
-        [HttpGet("Obtener-Todas-Tareas")]
+        /// <summary>
+        /// Obtiene todas las tareas del sistema.
+        /// </summary>
+        /// <returns>Lista de tareas con el nombre de su usuario asignado.</returns>
+        [HttpGet("ObtenerTodasTareas")]
         public IActionResult ObtenerTodasTareas()
         {
             var tareas = _servicio.ObtenerTodas();
@@ -35,56 +43,88 @@ namespace GestorDeTareas.Controllers
             return Ok(tareas);
         }
 
-        [HttpDelete("Eliminar-Tarea/{id}")]
+        /// <summary>
+        /// Elimina una tarea en especifico.
+        /// </summary>
+        /// <returns>No devuleve nada a no ser de que haya fallado.</returns>
+        [HttpDelete("EliminarTarea/{id}")]
         public IActionResult EliminarTarea(int id)
         {
             _servicio.EliminarTarea(id);
             return NoContent();
         }
 
-        [HttpPut("Actualizar-Completar-Tarea/{id}")]
+        /// <summary>
+        /// Actualiza el estado de una tarea a Completa.
+        /// </summary>
+        /// <returns>No devuleve nada a no ser que haya habido un fallo.</returns>
+        [HttpPut("ActualizarCompletarTarea/{id}")]
         public IActionResult CompletarTarea(int id)
         {
             _servicio.CompletarTarea(id);
             return NoContent();
         }
 
-        [HttpPut("Actualizar-Pendiente-Tarea/{id}")]
+        /// <summary>
+        /// Actualiza el estado de una tarea a Pendiente.
+        /// </summary>
+        /// <returns>No devuleve nada a no ser que haya habido un fallo.</returns>
+        [HttpPut("ActualizarPendienteTarea/{id}")]
         public IActionResult PendienteTarea(int id)
         {
             _servicio.PendienteTarea(id);
             return NoContent();
         }
 
-        [HttpPut("Actualizar-En-Progreso-Tarea/{id}")]
+        /// <summary>
+        /// Actualiza el estado de una tarea a EnProgreso.
+        /// </summary>
+        /// <returns>No devuleve nada a no ser que haya habido un fallo.</returns>
+        [HttpPut("ActualizarEnProgresoTarea/{id}")]
         public IActionResult EnProgresoTarea(int id)
         {
             _servicio.EnProgresoTarea(id);
             return NoContent();
         }
 
-        [HttpPut("Actualizar-Cancelar-Tarea/{id}")]
+        /// <summary>
+        /// Actualiza el estado de una tarea a Cancelada y se le da un motivo de cancelacion.
+        /// </summary>
+        /// <returns>No devuleve nada a no ser que haya habido un fallo.</returns>
+        [HttpPut("ActualizarCancelarTarea/{id}")]
         public IActionResult CancelarTarea(int id, string motivoCancelacion)
         {
             _servicio.CancelarTarea(id, motivoCancelacion);
             return NoContent();
         }
 
-        [HttpPut("Actualizar-Prioridad-Baja-Tarea/{id}")]
+        /// <summary>
+        /// Actualiza la Prioridad de una tarea a Baja.
+        /// </summary>
+        /// <returns>No devuleve nada a no ser que haya habido un fallo.</returns>
+        [HttpPut("ActualizarPrioridadBajaTarea/{id}")]
         public IActionResult PrioridadBajaTarea(int id)
         {
             _servicio.PrioridadBajaTarea(id);
             return NoContent();
         }
 
-        [HttpPut("Actualizar-Prioridad-Media-Tarea/{id}")]
+        /// <summary>
+        /// Actualiza la Prioridad de una tarea a Media.
+        /// </summary>
+        /// <returns>No devuleve nada a no ser que haya habido un fallo.</returns>
+        [HttpPut("ActualizarPrioridadMediaTarea/{id}")]
         public IActionResult PrioridadMediaTarea(int id)
         {
             _servicio.PrioridadMediaTarea(id);
             return NoContent();
         }
 
-        [HttpPut("Actualizar-Prioridad-Alta-Tarea/{id}")]
+        /// <summary>
+        /// Actualiza la Prioridad de una tarea a Alta.
+        /// </summary>
+        /// <returns>No devuleve nada a no ser que haya habido un fallo.</returns>
+        [HttpPut("ActualizarPrioridadAltaTarea/{id}")]
         public IActionResult PrioridadAltaTarea(int id)
         {
             _servicio.PrioridadMediaTarea(id);
--- GestorDeTareas/GestorDeTareas/Presentation/Program.cs
+++ GestorDeTareas/GestorDeTareas/Presentation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GestorDeTareas.Dominio.Entities;
 using GestorDeTareas.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using static GestorDeTareas.Dominio.Enums.Enumerados;
 
 
@@ -15,6 +16,16 @@ var listaTareas = new List<TareaAction> {
 var builder = WebApplication.CreateBuilder(args);
 
 //builder.Services.AddDbContext<GestorTareasContext> (options => options.UseSqlServer(builder.Configuration.GetConnectionString("GestorTareas")));
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly
+    .GetExecutingAssembly()
+    .GetName().Name}.xml";
+    var xmlPath = Path.Combine(
+    AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 // PARTE 1: registrar servicios
 builder.Services.AddControllers();
